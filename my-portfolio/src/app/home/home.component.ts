26df2de3d9b9a47d51c155ab238903a6b37a2c29
Deus Vult <75284235+Exokun14@users.noreply.github.com>
2024-02-21 10:51:00 +0800

    The greatest developer that's ever lived!

--- my-portfolio/src/app/home/home.component.ts
+++ my-portfolio/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +7,31 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
 
   @ViewChild('test', {static: true}) testRef!: ElementRef;
+  @ViewChild('scrollButtonHolder', {static: true}) scrollButtonHolderRef!: ElementRef;
+  @ViewChild('imageSlideshow', {static: true}) imageSlideshowRef!: ElementRef;
 
   updateTest() {
+
     console.log(this.testRef.nativeElement.innerHTML);
 
     this.testRef.nativeElement.innerHTML = 'The Greatest Developer That&#39s Ever Lived';
   }
+
+  
+  ngAfterViewInit() 
+  {
+    this.imageSlideshowRef.nativeElement.addEventListener('mouseenter', () => {
+      this.scrollButtonHolderRef.nativeElement.style.display = 'unset';
+    });
+
+    this.imageSlideshowRef.nativeElement.addEventListener('mouseleave', () => {
+      this.scrollButtonHolderRef.nativeElement.style.display = 'none';
+    });
+  }
+  
 
 }

1104c2f2f3dbdffa1643b36b1107abe946e34847
Deus Vult <75284235+Exokun14@users.noreply.github.com>
2024-02-23 11:38:07 +0800

    Chloe

--- my-portfolio/src/app/home/featured-component-a/featured-component-a.component.ts
+++ my-portfolio/src/app/home/featured-component-a/featured-component-a.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-featured-component-a',
@@ -9,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class FeaturedComponentAComponent {
 
+  @ViewChild ('test', {static: true}) testRef!: ElementRef;
+
+  ngAfterViewInit() {
+
+    /*
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // testRef is in the viewport
+          console.log('testRef is in the viewport');
+        } else {
+          // testRef is not in the viewport
+          console.log('testRef is not in the viewport');
+        }
+      });
+    });
+
+    observer.observe(this.testRef.nativeElement);
+  }
+
+  */
+}
 }
--- my-portfolio/src/app/home/home.component.ts
+++ my-portfolio/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 import { FeaturedComponentAComponent } from './featured-component-a/featured-component-a.component';
 
 @Component({

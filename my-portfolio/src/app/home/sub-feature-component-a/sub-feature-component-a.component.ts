1f9695dc93da776eb6057617c3b37e66c96f2c83
Deus Vult <75284235+Exokun14@users.noreply.github.com>
2024-02-28 17:13:37 +0800

    Revert "Test_2"
    This reverts commit 7bc2dd049fb80e55c5f889a7f6d80902090d2543.

---
+++ my-portfolio/src/app/home/sub-feature-component-a/sub-feature-component-a.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-feature-component-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './sub-feature-component-a.component.html',
+  styleUrl: './sub-feature-component-a.component.css'
+})
+export class SubFeatureComponentAComponent {
+
+}
---
+++ my-portfolio/src/app/home/sub-feature-component-b/sub-feature-component-b.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-feature-component-b',
+  standalone: true,
+  imports: [],
+  templateUrl: './sub-feature-component-b.component.html',
+  styleUrl: './sub-feature-component-b.component.css'
+})
+export class SubFeatureComponentBComponent {
+
+}
